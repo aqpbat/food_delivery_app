2e38b3e5f473c084f96f5490aa9895c73c2dab03
batyr <batyrakpanbet@gmail.com>
2024-07-08 02:36:43 +0500

    added themes: ligth and dark, added theme provider, added provider package

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 25),
+            Text(
+              'Food Delivery',
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            const SizedBox(height: 25),
+            TextField(),
+          ],
+        ),
+      ),
+    );
+  }
+}
